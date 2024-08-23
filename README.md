0c57312c6865bb820e7b366148e9b32a49004653
GRTeamBoss <rabbitwow97@gmail.com>
2024-08-23 14:11:44 +0500

    #RELEASE

---
+++ README.md
@@ -0,0 +1,10 @@
+# Invite Wedding Web-Site
+
+Template for create invite wedding websites
+
+## Contacts
+
+[Web-Site](https://invite.netlify.app)
+[Portfolio](https://grtbportfolio.netlify.app)
+[Telegram](https://t.me/grteamboss)
+[Instagram](https://instagram.com/grteamboss)
---
+++ main.js
@@ -0,0 +1,78 @@
+const ISOmilSec = 1000
+const ISOmilMinute = ISOmilSec * 60
+const ISOmilHour = ISOmilMinute * 60
+const ISOmilDay = ISOmilHour * 24
+
+
+
+const countdownDiv = document.getElementsByClassName("countdown-timer")[0]
+
+const weddingEl = document.getElementById("wedding-status")
+
+const dayEl = document.getElementById("day")
+const dayTitle = document.getElementById("day-title")
+const hourEl = document.getElementById("hour")
+const hourTitle = document.getElementById("hour-title")
+const minuteEl = document.getElementById("minute")
+const minuteTitle = document.getElementById("minute-title")
+const secondEl = document.getElementById("second")
+const secondTitle = document.getElementById("second-title")
+
+const targetDate = new Date("10.7.2024").getTime()
+
+const timer = setInterval(function() {
+  const todayDate = new Date().getTime()
+  const leftTime = targetDate - todayDate
+  if (leftTime <= 0) {
+    countdownDiv.style.display = "none"
+    if (leftTime > -8640000) {
+      weddingEl.textContent = "Сегодня свадьба!!!"
+    } else {
+      weddingEl.textContent = "Свадьба уже прошла :-("
+    }
+  clearInterval(timer)
+  } else {
+    dayEl.textContent = Math.floor(leftTime / ISOmilDay)
+    hourEl.textContent = Math.floor((leftTime % ISOmilDay) / ISOmilHour)
+    minuteEl.textContent = Math.floor((leftTime % ISOmilHour) / ISOmilMinute)
+    secondEl.textContent = Math.floor((leftTime % ISOmilMinute) / ISOmilSec)
+  }
+})
+
+const approveWillBeList = ["approve-yes", "approve-no"]
+const approveWithWhoList = ["approve-alone", "approve-friend", "approve-partner", "approve-family"]
+
+const approveInputEl = document.getElementById("approve-input")
+const approveYesEl = document.getElementById("approve-yes")
+const approveNoEl = document.getElementById("approve-no")
+const approveAloneEl = document.getElementById("approve-alone")
+const approveFriendEl = document.getElementById("approve-friend")
+const approvePartnerEl = document.getElementById("approve-partner")
+const approveFamilyEl = document.getElementById("approve-family")
+let approveWillBe = null
+let approveWithWho = "-"
+
+function sendApprove() {
+  const chatIds = [258598355, 53138192, 167628351]
+  // const chatIds = [167628351]
+  const fullName = approveInputEl.value
+  if (approveYesEl.checked === false) {
+    approveWillBe = "Нет"
+  } else {
+    approveWillBe = "Да"
+    if (approveAloneEl.checked !== false) {
+      approveWithWho = "Буду один / одна"
+    } else if (approveFriendEl.checked !== false) {
+      approveWithWho = "Буду с другом / подругой"
+    } else if (approvePartnerEl.checked !== false) {
+      approveWithWho = "Буду с парнем / девушкой"
+    } else {
+      approveWithWho = "Буду с семьей"
+    }
+  }
+  const text = `Ф.И.О: ${fullName}%0AБудет на свадьбе: ${approveWillBe}%0AС кем придет на свадьбу: ${approveWithWho}`
+  for (let chat of chatIds) {
+    const URI = `https://api.telegram.org/bot7282021975:AAGwtaeUslKML424Bq8QczgxkZBTIKp8L3M/sendMessage?chat_id=${chat}&text=${text}`
+    fetch(URI)
+  }
+}
